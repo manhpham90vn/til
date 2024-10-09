b79cc00aed7a966d906593e8070a9ad74f33138f
Manh Pham <manhpham90vn@icloud.com>
2024-10-09 16:58:55 +0700

    update

--- docker/nginx/deploy.sh
+++ docker/nginx/deploy.sh
@@ -1,4 +1,4 @@
 aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 047590809543.dkr.ecr.ap-southeast-1.amazonaws.com
-docker build -t web .
-docker tag web:latest 047590809543.dkr.ecr.ap-southeast-1.amazonaws.com/web:latest
-docker push 047590809543.dkr.ecr.ap-southeast-1.amazonaws.com/web:latest
+docker build -t nginx .
+docker tag nginx:latest 047590809543.dkr.ecr.ap-southeast-1.amazonaws.com/nginx:latest
+docker push 047590809543.dkr.ecr.ap-southeast-1.amazonaws.com/nginx:latest
