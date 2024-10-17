8b34b72836eb9f250ea87dbadc59a4ac9cfe16eb
Manh Pham <manhpham90vn@icloud.com>
2024-10-17 08:36:15 +0700

    update

--- docker/dockerize_expressjs/src/index.js
+++ docker/dockerize_expressjs/src/index.js
@@ -4,20 +4,29 @@ require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT;
+const env = process.env.NODE_ENV;
+const nodeVersion = process.version;
+const pid = process.pid;
 
 app.get("/", (req, res) => {
   res.json({
-    clientIP: req.headers["x-forwarded-for"] || null,
-    remoteAddress: req.socket.remoteAddress,
-    localAddress: req.socket.localAddress,
+    client_ip: req.headers["x-forwarded-for"] || null,
+    remote_address: req.socket.remoteAddress,
+    local_address: req.socket.localAddress,
     hostname: os.hostname(),
     platform: os.platform(),
     cpus: os.cpus().length,
+    env: env,
+    port: port,
+    node_version: nodeVersion,
+    app_version: 1,
+    date: new Date(),
+    pid: pid,
   });
 });
 
 app.listen(port, () => {
   console.log(
-    `Example app listening on port ${port}, env: ${process.env.NODE_ENV}`
+    `Example app listening on port ${port}, env: ${env}, pid: ${pid}`
   );
 });
